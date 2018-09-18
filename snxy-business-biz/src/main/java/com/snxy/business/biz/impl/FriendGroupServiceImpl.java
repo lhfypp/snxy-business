3bb94f83c2d860129fd830a85f8bebb6a26d4054
zhang33feng <zhangfeng_xfdsn@163.com>
2018-09-18 18:26:52 +0800

    添加了service和impl

---
+++ snxy-business-biz/src/main/java/com/snxy/business/biz/impl/FriendGroupServiceImpl.java
@@ -0,0 +1,10 @@
+package com.snxy.business.biz.impl;
+
+import com.snxy.business.service.FriendGroupService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class FriendGroupServiceImpl implements FriendGroupService {
+}
--- snxy-business-service/src/main/java/com/snxy/business/service/TestService.java
+++
@@ -1,7 +0,0 @@
-package com.snxy.business.service;
-
-/**
- * Created by 24398 on 2018/9/18.
- */
-public interface TestService {
-}
