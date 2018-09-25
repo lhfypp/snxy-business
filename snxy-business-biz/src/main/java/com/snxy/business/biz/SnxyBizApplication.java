c08af5501dd9b48f865082eee24b935b07839618
wei <changminwei@synjones.net>
2018-09-25 12:01:41 +0800

    '单元测试'

--- snxy-business-biz/src/main/java/com/snxy/business/biz/SnxyBizApplication.java
+++ snxy-business-biz/src/main/java/com/snxy/business/biz/SnxyBizApplication.java
@@ -3,12 +3,14 @@ package com.snxy.business.biz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by 24398 on 2018/9/3.
  */
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan({"com.snxy.business.biz","com.snxy.business.dao"})
 public class SnxyBizApplication {
     public static void main(String[] args) {
         SpringApplication.run(SnxyBizApplication.class, args);
