bf8a5f973261c3ac4134cec12925d2d0d1c6e962
luozhiwen <luozw2004@163.com>
2018-10-17 14:28:48 +0800

    商户发货订单

--- snxy-business-biz/src/main/java/com/snxy/business/biz/impl/DeliveryOrderServiceImpl.java
+++ snxy-business-biz/src/main/java/com/snxy/business/biz/impl/DeliveryOrderServiceImpl.java
@@ -1,10 +1,17 @@
 package com.snxy.business.biz.impl;
 
+import com.snxy.business.domain.DeliveryOrder;
 import com.snxy.business.service.DeliveryOrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 public class DeliveryOrderServiceImpl implements DeliveryOrderService {
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void createDeliveryOrder(DeliveryOrder deliveryOrder) {
+
+    }
 }
