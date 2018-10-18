a7a83cf4dc0907a0181447f157967c8b1f688165
e <2569018536@qq.com>
2018-10-18 18:57:48 +0800

    查询发货单信息

--- snxy-business-web/src/main/java/com/snxy/business/web/filter/DeliOrderFilter.java
+++ snxy-business-web/src/main/java/com/snxy/business/web/filter/DeliOrderFilter.java
@@ -1,13 +1,13 @@
 package com.snxy.business.web.filter;
 
-import com.snxy.common.util.StringUtil;
+
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 @Configuration
-@WebFilter(urlPatterns = "/delivery/seller/order/list ")
+@WebFilter(urlPatterns = "/delivery/seller/order/list")
 public class DeliOrderFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -16,14 +16,9 @@ public class DeliOrderFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String userId=servletRequest.getParameter("userId");
-        String orderStatus=servletRequest.getParameter("servletRequest");
-        String serchName=servletRequest.getParameter("serchName");
-        if(StringUtil.isBlank(userId)){
-            servletRequest.getRequestDispatcher("/failed").forward(servletRequest, servletResponse);
-        }else{
+
             filterChain.doFilter(servletRequest, servletResponse);
-        }
+
     }
 
     @Override
