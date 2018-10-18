c46b0648e94a5ae9d7490cfc9ad2a46254abf7b8
e <2569018536@qq.com>
2018-10-18 20:59:33 +0800

    查询发货单信息

--- snxy-business-web/src/main/java/com/snxy/business/web/SnxyWebApplication.java
+++ snxy-business-web/src/main/java/com/snxy/business/web/SnxyWebApplication.java
@@ -2,12 +2,14 @@ package com.snxy.business.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringCloudApplication
 @EnableEurekaClient
+
 @ComponentScan({"com.snxy.business.web","com.snxy.business.biz","com.snxy.business.dao"})
 public class SnxyWebApplication {
 
