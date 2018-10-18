362d2c4218cba39f4d1e89029ac241effeeeea41
e <2569018536@qq.com>
2018-10-18 16:26:35 +0800

    查询发货单信息

--- snxy-business-web/src/main/java/com/snxy/business/web/controller/DeliveryOrderController.java
+++ snxy-business-web/src/main/java/com/snxy/business/web/controller/DeliveryOrderController.java
@@ -1,5 +1,6 @@
 package com.snxy.business.web.controller;
 
+import com.snxy.business.domain.BillInfo;
 import com.snxy.business.domain.DeliveryOrder;
 import com.snxy.business.service.DeliveryOrderService;
 import com.snxy.common.response.ResultData;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /*
@@ -29,5 +33,23 @@ public class DeliveryOrderController {
         deliveryOrderService.createDeliveryOrder(deliveryOrder);
 
         return ResultData.success("发货订单新建成功");
+    }
+    @RequestMapping(value = "/seller/order/list")
+    public ResultData<List<BillInfo> >searchDeliveryOrder(HttpServletRequest request){
+        long userId=Long.parseLong(request.getParameter("userId"));//用户标识
+        String orderStatus=request.getParameter("orderStatus");//订单状态
+        String serchName=request.getParameter("serchName");//地点or联系人or单号
+
+
+        ResultData<List<BillInfo>>listResultData=new ResultData<List<BillInfo>>();
+        listResultData.setData(deliveryOrderService.searchDeliveryOrder(userId,orderStatus,serchName));
+
+     return listResultData;
+    }
+    @RequestMapping("/failed")
+    public ResultData  requestFailed(){
+
+
+        return ResultData.fail("用户id为空");
     }
 }
