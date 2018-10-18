9d37bc4f2914789d9f6c05369102a8bf386905c8
e <2569018536@qq.com>
2018-10-18 16:23:33 +0800

    查询发货单信息

--- snxy-business-dao/src/main/java/com/snxy/business/dao/mapper/DeliveryOrderMapper.java
+++ snxy-business-dao/src/main/java/com/snxy/business/dao/mapper/DeliveryOrderMapper.java
@@ -1,6 +1,10 @@
 package com.snxy.business.dao.mapper;
 
+import com.snxy.business.domain.BillInfo;
 import com.snxy.business.domain.DeliveryOrder;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface DeliveryOrderMapper {
     int deleteByPrimaryKey(Long id);
@@ -14,4 +18,5 @@ public interface DeliveryOrderMapper {
     int updateByPrimaryKeySelective(DeliveryOrder record);
 
     int updateByPrimaryKey(DeliveryOrder record);
+    List<BillInfo> searchDeliveryOrder(@Param("sendPhones") List<String >sendPhones,@Param("orderStatus") String orderStatus,@Param("serchName") String serchName);
 }
