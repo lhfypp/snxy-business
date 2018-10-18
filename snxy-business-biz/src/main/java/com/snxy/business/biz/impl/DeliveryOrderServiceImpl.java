19dba1e0b714b68219d9be9d873524d07fb09a58
e <2569018536@qq.com>
2018-10-18 17:30:03 +0800

    查询发货单信息

--- snxy-business-biz/src/main/java/com/snxy/business/biz/impl/DeliveryOrderServiceImpl.java
+++ snxy-business-biz/src/main/java/com/snxy/business/biz/impl/DeliveryOrderServiceImpl.java
@@ -1,17 +1,55 @@
 package com.snxy.business.biz.impl;
 
-import com.snxy.business.domain.DeliveryOrder;
+import com.snxy.business.dao.mapper.*;
+import com.snxy.business.domain.*;
 import com.snxy.business.service.DeliveryOrderService;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
 public class DeliveryOrderServiceImpl implements DeliveryOrderService {
+    @Resource
+    private SystemUserInfoMapper systemUserInfoMapper;
+    @Resource
+    private DeliveryOrderMapper deliveryOrderMapper;
+
+    @Resource
+    private VegetableCertificateMapper vegetableCertificateMapper;
+
+    @Resource
+    private VegetableImageMapper vegetableImageMapper;
+
+    @Resource
+    private VegetableDeliveryRelationMapper vegetableDeliveryRelationMapper;
+
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public void createDeliveryOrder(DeliveryOrder deliveryOrder) {
+    public void createDeliveryOrder(DeliveryOrder deliveryOrder, VegetableDeliveryRelation vegetableDeliveryRelation, VegetableCertificate vegetableCertificate, VegetableImage vegetableImage) {
+        deliveryOrderMapper.insertSelective(deliveryOrder);
+        Long id = deliveryOrder.getId();
 
+        vegetableCertificate.setDeliveryOrderId(id);
+        vegetableCertificateMapper.insertSelective(vegetableCertificate);
+
+        vegetableImage.setDeliveryOrderId(id);
+        vegetableImageMapper.insertSelective(vegetableImage);
+
+        vegetableDeliveryRelation.setDeliveryOrderId(id);
+        vegetableDeliveryRelationMapper.insertSelective(vegetableDeliveryRelation);
+    }
+    @Override
+    public List<BillInfo> searchDeliveryOrder(Long useId, String orderStatus, String serchName) {
+
+        //用于存放商户或者代办所有的手机信息
+        List<String>sendPhones=new ArrayList<String>();
+        sendPhones= systemUserInfoMapper.searchPhones(useId);
+        return  deliveryOrderMapper.searchDeliveryOrder(sendPhones,orderStatus,serchName);
     }
 }
