51996cc4098e4e167409baa7a54925bd3755fe23
e <2569018536@qq.com>
2018-10-18 17:31:03 +0800

    查询发货单信息

--- snxy-business-service/src/main/java/com/snxy/business/service/DeliveryOrderService.java
+++ snxy-business-service/src/main/java/com/snxy/business/service/DeliveryOrderService.java
@@ -1,7 +1,10 @@
 package com.snxy.business.service;
 
-import com.snxy.business.domain.DeliveryOrder;
+import com.snxy.business.domain.*;
+
+import java.util.List;
 
 public interface DeliveryOrderService {
-    void createDeliveryOrder(DeliveryOrder deliveryOrder);
+    void createDeliveryOrder(DeliveryOrder deliveryOrder, VegetableDeliveryRelation vegetableDeliveryRelation, VegetableCertificate vegetableCertificate, VegetableImage vegetableImage);
+    List<BillInfo>  searchDeliveryOrder(Long useId, String orderStatus, String serchName);
 }
