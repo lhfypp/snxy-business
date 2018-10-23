f6494a6992c2cc86bcbd22a8b2f8347abd4ec016
e <2569018536@qq.com>
2018-10-23 12:23:28 +0800

    订单信息

--- snxy-business-service/src/main/java/com/snxy/business/service/DeliveryOrderService.java
+++ snxy-business-service/src/main/java/com/snxy/business/service/DeliveryOrderService.java
@@ -1,25 +1,24 @@
 package com.snxy.business.service;
 
+import com.github.pagehelper.PageInfo;
 import com.snxy.business.domain.*;
-import com.snxy.common.util.PageInfo;
 
 import java.util.List;
-
+import java.util.Map;
 
 public interface DeliveryOrderService {
-
-    void createDeliveryOrder(DeliveryOrderVo deliveryOrderVo);
-
-    String getOrderNo();
-
-    List<BillInfo> selectDriverOrder(Long userId);
-
-    DriverOrderInfo selectOrderByOrderId(Long orderId);
-
     void createDeliveryOrder(DeliveryOrder deliveryOrder, VegetableDeliveryRelation vegetableDeliveryRelation, VegetableCertificate vegetableCertificate, VegetableImage vegetableImage);
     List<BillInfo>  searchDeliveryOrder( String orderStatus, String searchName);
-    BillInfoDetail searchDeliverOrderinfo(Long deliveryOrderId);
+    BillInfoDetail searchDeliverOrderinfo(String deliveryOrderId);
     PageInfo<BillInfo> searchDeliveryOrderByPage(String orderStatus, String searchName);
-    void cancelOrder(Long orderId);
+    void cancelOrder(String  orderId);
+    void changeDriver(String orderId,String driverName,String drivePhone);
+    void updateOrder(UpdateBillInfoDetail billInfoDetail);
+    void acceptOrder(String orderId);//司机接受订单有Id
 
+    Map<String ,Object> getVehiclesForDriver(String OrderId);
+    void insertCurrOrderReceiver(String OrderId,String vehicleId);
+    void updateCurrOrderReceiver(String OrderId,String vehicleId);
+    void resufedOrder(String orderId);
+    void tranferOrder(String orderId,String driveMobile ,String driverName);
 }
