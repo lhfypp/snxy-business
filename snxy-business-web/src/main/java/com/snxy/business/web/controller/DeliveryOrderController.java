c67462a5073a83d029c36808faa1fec3a8d76c35
e <2569018536@qq.com>
2018-10-30 15:33:09 +0800

    查询蔬菜列表

--- snxy-business-web/src/main/java/com/snxy/business/web/controller/DeliveryOrderController.java
+++ snxy-business-web/src/main/java/com/snxy/business/web/controller/DeliveryOrderController.java
@@ -2,18 +2,21 @@ package com.snxy.business.web.controller;
 
 import com.snxy.business.domain.*;
 import com.snxy.business.service.*;
-import com.snxy.business.service.vo.DeliveryOrderVo;
+import com.snxy.business.service.vo.*;
 import com.snxy.common.response.ResultData;
+import com.snxy.common.util.PageInfo;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.hibernate.validator.constraints.NotBlank;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -36,43 +39,41 @@ public class DeliveryOrderController {
     private CompanyUserRelationService companyUserRelationService;
 
     @Resource
-    private SystemUserService systemUserService;
-
-    @Resource
     private VehicleGpsRecordService vehicleGpsRecordService;
 
 
     //货品信息添加页展示
     @RequestMapping(value = "/seller/showGoods")
     public ResultData showGoods (Long userCompanyId){
-        List<CompanyVegetable> companyVegetableList =  companyVegetableService.showGoods(1L);
-        return ResultData.success(companyVegetableList);
+        List<GoodsVo> goodsVoList =  companyVegetableService.showGoods(userCompanyId);
+        return ResultData.success(goodsVoList);
     }
 
     //货品信息搜索
     @RequestMapping(value = "/seller/selectByGoodsName")
-    public ResultData selectByGoodsName (String goodsName){
-        CompanyVegetable companyVegetable = companyVegetableService.selectByGoodsName("大白菜");
+    public ResultData selectCompanyGoodsByGoodsNameAndCompanyId (CompanyGoodsVo companyGoodsVo){
+        CompanyVegetable companyVegetable = companyVegetableService.selectCompanyGoodsByGoodsNameAndCompanyId(companyGoodsVo);
         return ResultData.success(companyVegetable);
     }
 
-    //新建发货订单
+    //新建发货订单的权限判断
     @RequestMapping(value = "/seller/bill/new")
-    public ResultData createDeliveryOrder (Integer deviceType,DeliveryOrderVo deliveryOrderVo){
+    public ResultData createDeliveryOrder (OnlineUserVo onlineUserVo){
         //先进行权限判断，是否为商户或者代办，公司信息是否完整，设置商户，代办身份标识为0，1
-        deviceType = 1;
-        if(deviceType!=0 && deviceType!=1){
-            return ResultData.fail("你还没有完成认证");
+        if(onlineUserVo.getIdentyType()!=0 && onlineUserVo.getIdentyType()!=1){
+            return ResultData.fail("您不是商户或代办，没有新建发货订单权限");
         }
         //查询是否完成公司信息填写
-        List<Long> list = companyUserRelationService.selectCompanyIsExist(1L);
+        List<Long> list = companyUserRelationService.selectCompanyIsExist(onlineUserVo.getOnlineUserId());
         if(list==null || list.size()==0){
             return ResultData.fail("你还没有完成认证");
         }
 
         //如果已认证则新建一个订单,此处系统生成订单号
 
+        DeliveryOrderVo deliveryOrderVo = new DeliveryOrderVo();
         String orderNo = deliveryOrderService.getOrderNo();
+
 
         deliveryOrderVo.setOrderNo(orderNo);
         return ResultData.success(deliveryOrderVo);
@@ -81,32 +82,8 @@ public class DeliveryOrderController {
     //保存发布订单
     @RequestMapping(value = "/seller/bill/save")
     public ResultData saveDeliveryOrder (DeliveryOrderVo deliveryOrderVo){
-
-        deliveryOrderService.createDeliveryOrder(deliveryOrderVo);
-
-
-        //发布订单对司机手机号进行判断是否注册
-        SystemUser systemUser = systemUserService.selectByMobile(deliveryOrderVo.getDriverMobile());
-        if (systemUser==null){
-            //当查询不到司机的手机号注册信息时给司机手机号发送app下载了短信链接
-
-        }else {
-            //如果司机已经注册则app推送订单消息
-        }
-        return ResultData.success("订单发布成功");
-    }
-
-    // 查看订单列表
-    @RequestMapping(value = "/seller/order/list")
-    public ResultData<List<BillInfo>> searchDeliveryOrder(HttpServletRequest request) {
-        String orderStatus = request.getParameter("orderStatus");//订单状态
-        String searchName = request.getParameter("searchName");//地点or联系人or单号
-
-
-        ResultData<List<BillInfo>> listResultData = new ResultData<List<BillInfo>>();
-        listResultData.setData(deliveryOrderService.searchDeliveryOrder(orderStatus, searchName));
-
-        return listResultData;
+        deliveryOrderService.saveDeliveryOrder(deliveryOrderVo);
+        return ResultData.success("");
     }
 
 
@@ -118,11 +95,13 @@ public class DeliveryOrderController {
         return ResultData.success(driverOrders);
     }
 
+
+
     //查看订单详情
     @RequestMapping(value = "/seller/order/detail")
-    public ResultData<BillInfoDetail > searchDeliveryOrderInfo(HttpServletRequest request) {
-        long deliveryOrderId= Long.parseLong(request.getParameter("deliveryOrderId"));
-        return ResultData.success (deliveryOrderService.searchDeliverOrderinfo(deliveryOrderId));
+    public ResultData<BillInfoDetail > searchDeliveryOrderInfo(@NotBlank(message="订单id不能为空") String deliveryOrderId) {
+        long orderId= Long.parseLong(deliveryOrderId);
+        return ResultData.success (deliveryOrderService.searchDeliverOrderinfo(orderId));
 
     }
 
@@ -135,13 +114,13 @@ public class DeliveryOrderController {
     }
    //商户取消订单
     @RequestMapping("/seller/bill/cancel")
-    public ResultData cancelOrder(HttpServletRequest request){
-        long logisticOrderId= Long.parseLong(request.getParameter("logisticOrderId"));
+    public ResultData cancelOrder(String logisticOrderId){
+        long orderId= Long.parseLong(logisticOrderId);
         //从用户对象获取
         String identityName="1";//商户
         if("1".equals(identityName)){
 
-        deliveryOrderService.cancelOrder(logisticOrderId,1);
+        deliveryOrderService.cancelOrder(orderId,1);
         return ResultData.success("商户取消订单成功");
         }else{
             return ResultData.fail("该用户没有权限取消订单");
@@ -151,15 +130,14 @@ public class DeliveryOrderController {
     //司机提示装货完毕
     @RequestMapping("/driver/endLoading")
     public ResultData driverEndLoading(Long deliveryOrderId){
-        deliveryOrderService.updateEndLoading(deliveryOrderId);
-
+        deliveryOrderService.updateEndLoading(deliveryOrderId,3);
         return ResultData.success("");
     }
 
     //上传GPS
     @RequestMapping("/driver/location/upload")
-    public ResultData uploadLocation(VehicleGpsRecord vehicleGpsRecord,Long userId){
-        vehicleGpsRecordService.insertLocationGPS(vehicleGpsRecord,userId);
+    public ResultData uploadLocation(VehicleGpsVo vehicleGpsVo){
+        vehicleGpsRecordService.insertLocationGPS(vehicleGpsVo);
         return ResultData.success("");
     }
 
@@ -172,12 +150,13 @@ public class DeliveryOrderController {
 
     //卖家或者代办换司机
     @RequestMapping("/seller/order/changeDriver")
-    public  ResultData<String> changeDriver(@RequestParam @NotBlank(message="订单号不能为空") String logisticOrderId, @RequestParam String driverMobile, @RequestParam String driverName){
-        deliveryOrderService.changeDriver(logisticOrderId,driverName,driverMobile);
+    public  ResultData<String> changeDriver(ChangeDriversVo changeDriversVo){
+        deliveryOrderService.changeDriver(changeDriversVo.getLogisticOrderId(),changeDriversVo.getDriverName(),changeDriversVo.getDriverMobile());
         return ResultData.success("卖家或者代办更换司机成功");
 
     }
     // 关闭订单 合格关闭和不合格关闭（是否与关闭订单合并?,还是三个按钮分别请求）
+    @RequestMapping("/seller/order/closeOrder")
     public ResultData<? extends Object> closeOrder(@NotBlank(message="订单id不能为空")@RequestParam String logisticOrderId ){
 
         //从用户对象获取
@@ -191,7 +170,8 @@ public class DeliveryOrderController {
         }
     }
     //修改订单
-    public ResultData<? extends Object> updateOrder(@Valid @RequestBody UpdateBillInfoDetail billInfoDetail){
+    @RequestMapping("/seller/order/updateOrder")
+    public ResultData<? extends Object> updateOrder(@Valid @RequestBody UpdateBillInfoDetailVo billInfoDetail){
         deliveryOrderService.updateOrder(billInfoDetail);
         return ResultData.success("修改订单成功");
 
@@ -207,11 +187,10 @@ public class DeliveryOrderController {
 
     //司机选择车辆 车牌号，和orderNO确定
     @RequestMapping("/driver/vehicle/choose")
-    public ResultData ChooseVehicle(@RequestParam @NotBlank(message = "订单id不能为空")String OrderId,@RequestParam @NotBlank(message="车类型id")String vehicleId){
+    public ResultData chooseVehicle(ChooseVehicle chooseVehicle){
 
-        deliveryOrderService.acceptOrder(OrderId);//把状态改为2
-        //  deliveryOrderService.insertCurrOrderReceiver(OrderId,vehicleId);//把信息插入curr_order_receiver表
-        deliveryOrderService.updateCurrOrderReceiver(OrderId,vehicleId);//司机选择车辆信息
+        deliveryOrderService.acceptOrder(chooseVehicle.getOrderId());//把状态改为2
+        deliveryOrderService.updateCurrOrderReceiver(chooseVehicle.getOrderId(),chooseVehicle.getVehicleId());//司机选择车辆信息
         return ResultData.success("选择车辆成功");
     }
     //司机拒绝接单
@@ -227,4 +206,17 @@ public class DeliveryOrderController {
     {
         deliveryOrderService.tranferOrder(deliveryOrderId,driverMobile,driverName);
     }
+
+    //查看订单列表,实现分页查询
+    @RequestMapping(value = "/seller/order/list")
+    public ResultData<PageInfo<BillInfo>>searchDeliveryOrderbypage(@Valid OrderListVo orderListVo){
+
+        PageInfo<BillInfo> pageInfo;
+        String orderStatus = orderListVo.getOrderStatus();//订单状态
+        String searchName = orderListVo.getSearchName();//地点or联系人or单号
+        pageInfo=deliveryOrderService.searchDeliveryOrderByPage(orderStatus, searchName);
+        return ResultData.success(pageInfo);
+    }
+
+
 }
