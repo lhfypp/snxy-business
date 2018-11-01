5d3fc57a1121c244d1d48bd3e10c768814d7128c
e <2569018536@qq.com>
2018-11-01 11:57:19 +0800

    查询蔬菜列表

--- snxy-business-service/src/main/java/com/snxy/business/service/SystemUserInfoService.java
+++ snxy-business-service/src/main/java/com/snxy/business/service/SystemUserInfoService.java
@@ -2,8 +2,11 @@ package com.snxy.business.service;
 
 
 
+import com.snxy.business.domain.SystemUserInfo;
+
 import java.util.List;
 
 public interface SystemUserInfoService {
     List<String> searchPhones( String onlineUserID);
+
 }
