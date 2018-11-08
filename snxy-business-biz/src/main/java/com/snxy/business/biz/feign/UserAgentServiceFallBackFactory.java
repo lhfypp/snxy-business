defd82891b8dc2be04e011b8729f915faca30339
e <2569018536@qq.com>
2018-11-08 10:40:22 +0800

    加入商户

--- snxy-business-biz/src/main/java/com/snxy/business/biz/feign/UserAgentServiceFallBackFactory.java
+++ snxy-business-biz/src/main/java/com/snxy/business/biz/feign/UserAgentServiceFallBackFactory.java
@@ -1,10 +1,7 @@
 package com.snxy.business.biz.feign;
 
-<<<<<<< HEAD
-import com.snxy.business.service.vo.SystemUserVO;
-import com.snxy.common.response.ResultData;
-=======
->>>>>>> 0715307c3886f405043faeee6e33e8c66c8ff20e
+
+
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -13,12 +10,9 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class UserAgentServiceFallBackFactory implements UserAgentService {
     @Override
-<<<<<<< HEAD
-    public ResultData<SystemUserVO> login(LoginUserVO loginUserVO) {
-        return ResultData.fail("登陆失败 ："+msg);
-=======
+
     public void refresh(Long systemUserId) {
         log.error("刷新用户缓存信息失败");
->>>>>>> 0715307c3886f405043faeee6e33e8c66c8ff20e
+
     }
 }
