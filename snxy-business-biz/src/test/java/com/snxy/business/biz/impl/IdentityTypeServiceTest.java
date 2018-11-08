da2a2bc0810bea0d830f6dd3cf710435fd6ef6c3
e <2569018536@qq.com>
2018-11-08 09:17:06 +0800

    加入商户

--- snxy-business-biz/src/test/java/com/snxy/business/biz/impl/IdentityTypeServiceTest.java
+++ snxy-business-biz/src/test/java/com/snxy/business/biz/impl/IdentityTypeServiceTest.java
@@ -1,5 +1,9 @@
 package com.snxy.business.biz.impl;
 
+import com.snxy.business.dao.mapper.IdentityTypeMapper;
+import com.snxy.business.dao.mapper.SystemUserMapper;
+
+import com.snxy.business.service.IdentityTypeService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.util.List;
+
 
 /**
  * Created by 24398 on 2018/9/25.
@@ -28,27 +32,27 @@ public class IdentityTypeServiceTest {
 
     @Test
     public void listAllTest1(){
-        List<IdentityType> identityTypeList = this.identityTypeService.listAll(true);
-        log.info("identityTypeList ----> : [{}]",identityTypeList);
+     //  List<IdentityType> identityTypeList = this.identityTypeService.listAll(true);
+     //   log.info("identityTypeList ----> : [{}]",identityTypeList);
     }
 
     @Test
     public void listAllTest2(){
-      List<IdentityType> identityTypes = this.identityTypeMapper.listAll((byte) 1);
-      log.info("identityTypes  ---> [{}]",identityTypes);
+     // List<IdentityType> identityTypes = this.identityTypeMapper.listAll((byte) 1);
+     // log.info("identityTypes  ---> [{}]",identityTypes);
     }
 
     @Test
      public void listAllTest3(){
-        List<IdentityType> identityTypes = this.identityTypeMapper.listAll((byte) 0);
-        log.info("identityTypes ----> : [{}]",identityTypes);
+       // List<IdentityType> identityTypes = this.identityTypeMapper.listAll((byte) 0);
+     //  log.info("identityTypes ----> : [{}]",identityTypes);
      }
 
 
     @Test
     public void systemUserMapperTest(){
-        SystemUser systemUser = this.systemUserMapper.selectByPrimaryKey(1L);
-        log.info("systemUser ---> [{}]",systemUser);
+       // SystemUser systemUser = this.systemUserMapper.selectByPrimaryKey(1L);
+       // log.info("systemUser ---> [{}]",systemUser);
     }
 
 
