6f3dc4cf68543426b4a2c556e31ab00e5108ee80
e <2569018536@qq.com>
2018-11-08 09:11:20 +0800

    加入商户

---
+++ snxy-business-service/src/main/java/com/snxy/business/service/CompanyUserRelationService.java
@@ -0,0 +1,7 @@
+package com.snxy.business.service;
+
+import com.snxy.business.domain.CompanyUserRelation;
+
+public interface CompanyUserRelationService {
+    int insert(CompanyUserRelation record);
+}
