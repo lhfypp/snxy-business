b48c841a2256b3890452829922b19887c650e283
e <2569018536@qq.com>
2018-11-08 10:39:16 +0800

    加入商户

--- snxy-business-domain/src/main/java/com/snxy/business/domain/CompanyUserRelation.java
+++ snxy-business-domain/src/main/java/com/snxy/business/domain/CompanyUserRelation.java
@@ -2,18 +2,18 @@ package com.snxy.business.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-<<<<<<< HEAD
+
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
-=======
+
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.util.Date;
+
+
 
 @Data
->>>>>>> 0715307c3886f405043faeee6e33e8c66c8ff20e
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
