6254280770b32343519172f2be15617f019662ac
xueqinyao168 <13611163216@.com>
2018-11-14 15:46:35 +0800

    删除userController合并到personalCenterController

--- snxy-business-web/src/main/java/com/snxy/business/web/controller/PersonalCenterController.java
+++ snxy-business-web/src/main/java/com/snxy/business/web/controller/PersonalCenterController.java
@@ -2,8 +2,11 @@ package com.snxy.business.web.controller;
 
 
 
+import com.snxy.business.biz.feign.FileService;
 import com.snxy.business.service.OnlineUserService;
 import com.snxy.business.service.PerSettingsHomepageService;
+import com.snxy.business.service.UserImageService;
+import com.snxy.business.service.VersionService;
 import com.snxy.business.service.vo.CompanyVO;
 import com.snxy.business.service.vo.SystemUserVO;
 import com.snxy.common.response.ResultData;
@@ -13,9 +16,13 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 /**
@@ -32,6 +39,13 @@ public class PersonalCenterController {
     private PerSettingsHomepageService PerSettingsHomepageService;
     @Resource
     private OnlineUserService onlineUserService;
+    @Resource
+    private UserImageService userImageService;
+    @Resource
+    private FileService fileService;
+    @Resource
+    private VersionService versionService;
+
     //设置所属商户(搜索)
     @RequestMapping("/company/search")
     public ResultData setTheownerMerchant( String companyName){
@@ -59,5 +73,48 @@ public class PersonalCenterController {
     public ResultData handleCardReserve(){
         log.info("通知中信预约办卡");
         return null;
+    }
+    /**
+     * 修改用户头像
+     *
+     * @param systemUserId
+     * @param file
+     */
+    @RequestMapping("/updateUserImage")
+    public void updateUserImage(Long systemUserId, MultipartFile file) {
+        log.info("============================");
+        userImageService.updateImageById(systemUserId, file);
+    }
+    /**
+     * 下载版本
+     * @param systemUserId
+     * @param response
+     */
+    @RequestMapping("/download")
+    public void  downLoad(Long systemUserId, HttpServletResponse response) {
+        try {
+            //下载首先是获取服务器地址，而发版的地址放到数据库中
+            String url = versionService.selectUrlBySystemUserId(systemUserId);
+            ResultData<byte[]> data = fileService.download(url);
+            byte[] buf = data.getData();
+            // 创建输出流对象
+            OutputStream outStream = response.getOutputStream();
+            // 开始输出
+            outStream.write(buf, 0, buf.length);
+            // 关闭流对象
+            outStream.close();
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    /**
+     * 发布版本
+     * @param systemUserId
+     * @param versionNum
+     * @param file
+     */
+    @RequestMapping("/release")
+    public void release(Long systemUserId, String versionNum, MultipartFile file){
+        versionService.release(systemUserId,versionNum,file);
     }
 }
